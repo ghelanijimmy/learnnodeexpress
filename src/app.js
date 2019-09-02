611d63daa3f916693af9a0b117aa112dbf0b2632
Jimmy Ghelani <ghelanijimmy@jgdesigns.ca>
2019-09-02 10:05:44 -0400

    updated gitignore

--- src/app.js
+++ src/app.js
@@ -3,9 +3,9 @@
 // const os = require("os");
 // const fs = require("fs");
 // const http = require("http");
-const express = require("express");
 const Joi = require("@hapi/joi");
 
+const express = require("express");
 // const pathObj = path.parse(__filename);
 // const totalMemory = os.totalmem();
 // const freeMemory = os.freemem();
@@ -53,6 +53,15 @@ const Joi = require("@hapi/joi");
 const app = express();
 app.use(express.json());
 
+const validateCourse = course => {
+  const schema = {
+    name: Joi.string()
+      .min(3)
+      .required()
+  };
+  return Joi.validate(course, schema);
+};
+
 app.get("/", (req, res) => {
   res.send("hello world");
 });
@@ -60,7 +69,7 @@ app.get("/", (req, res) => {
 const courses = [
   { id: 1, name: "course1" },
   { id: 2, name: "course2" },
-  { id: 2, name: "course3" }
+  { id: 3, name: "course3" }
 ];
 
 app.get("/api/courses", (req, res) => {
@@ -75,23 +84,50 @@ app.get("/api/courses/:id", (req, res) => {
 });
 
 app.post("/api/courses", (req, res) => {
-  let schema = {
-    name: Joi.string()
-      .min(3)
-      .required()
-  };
+  const { error } = validateCourse(req.body);
 
-  const result = Joi.validate(req.body, schema);
-
-  if (!req.body.name || req.body.name.length < 3) {
-    res.status(400).send(result.error);
+  if (error) {
+    res.status(400).send(error.details[0].message);
     return;
   }
+
   const course = {
     id: courses.length + 1,
     name: req.body.name
   };
   courses.push(course);
+  res.send(course);
+});
+
+app.put("/api/courses/:id", (req, res) => {
+  //Look for course
+  const course = courses.find(c => c.id === parseInt(req.params.id));
+  if (!course)
+    res
+      .status(404)
+      .send(`The course with the id ${req.params.id} doesn't exist`);
+
+  //validate
+  const { error } = validateCourse(req.body);
+  if (error) {
+    res.status(400).send(error.details[0].message);
+    return;
+  }
+
+  course.name = req.body.name;
+  res.send(course);
+});
+
+app.delete("/api/courses/:id", (req, res) => {
+  const course = courses.find(c => c.id === parseInt(req.params.id));
+  if (!course)
+    res
+      .status(404)
+      .send(`The course with the id ${req.params.id} doesn't exist`);
+
+  const index = courses.indexOf(course);
+  courses.splice(index, 1);
+
   res.send(course);
 });
 
