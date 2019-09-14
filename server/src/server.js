24150022132537c82539b61d357af8ca0662a96c
Jimmy Ghelani <ghelanijimmy@jgdesigns.ca>
2019-09-14 00:04:58 -0400

    made dockerfile and docker-compose file for creating container and image

--- server/src/server.js
+++ server/src/server.js
@@ -14,7 +14,7 @@ const uri = process.env.ATLAS_URI;
 mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true });
 const connection = mongoose.connection;
 connection.once("open", () => {
-  console.log("MongoDB database connection established successfully");
+  console.log("MongoDB database connection established successfully!");
 });
 
 const exerciseRouter = require("./routes/exercises");
